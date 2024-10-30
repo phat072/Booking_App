8e42d7d491318cd33a8e6b088b3f486431094eae
phat072 <phanphatlqd2003@gmail.com>
2024-10-30 13:11:06 +0700

    fixed user

--- api/index.js
+++ api/index.js
@@ -1,13 +1,44 @@
+// app.js
 const express = require('express');
 const connectedDB = require('./configs/database'); 
 
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const port = 8000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 connectedDB();
+app.use("/api", routes);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the application!');
-});
-
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "My API",
+        version: "1.0.0",
+        description: "A simple Express API",
+      },
+      servers: [
+        {
+          url: "https://booking-res.vercel.app", // URL của server đã triển khai lên Vercel
+        },
+      ],
+    },
+    apis: ['./routes/routes.js'], // Đường dẫn tới file chứa các comment swagger
+  };
+  
+  
+  const swaggerSpec = swaggerJsdoc(options);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
---
+++ api/routes/routes.js
@@ -0,0 +1,12 @@
+// routes.js
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+
+router.post("/register", userController.register);
+router.get("/verify/:token", userController.verifyEmail);
+router.post("/login", userController.login);
+router.put("/address/:userId", userController.updateAddress);
+router.get("/address/:userId", userController.getUserAddress);
+router.put("/change-password/:userId", userController.changePassword);
